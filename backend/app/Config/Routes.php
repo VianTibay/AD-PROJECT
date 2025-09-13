6cf66ff98c51d0aae690c7fb3fe74fdcde87b5f4
Vian Tibay <202310591@fit.edu.ph>
2025-09-14 05:08:01 +0800

    fix(routes): Restore home route by removing user route

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -5,11 +5,5 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-<<<<<<< HEAD
-// $routes->get('/', 'Home::index');
-$routes->get('/user','User::index');
 
-
-=======
 $routes->get('/', 'Home::index');
->>>>>>> parent of 75716bb (feat(landing page): Added a new landing page)
