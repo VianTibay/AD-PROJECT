3305e3e0b625052c67d9d22926d3c308ddc145a8
Vian Tibay <202310591@fit.edu.ph>
2025-09-14 06:47:39 +0800

    feat(login): Add login page with form and navigation links

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -6,6 +6,12 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
-$routes->get('/user', 'User::landing');
 $routes->get('/', 'Home::index');
+$routes->get('/user', 'User::landing');
+$routes->get('/user/login', 'User::login');
+$routes->get('/user/signup', 'User::signup');
+$routes->get('/user/moodboard', 'User::moodBoard');
+$routes->get('/landing', 'User::landing');
+$routes->get('/login', 'User::login');
+
 
