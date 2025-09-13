352d13312c70e1891091511c3c968a9bdd51654e
Vian Tibay <202310591@fit.edu.ph>
2025-09-14 06:52:31 +0800

    feat(user): Add user login, signup, moodboard, and roadmap methods

--- backend/app/Controllers/User.php
+++ backend/app/Controllers/User.php
@@ -10,6 +10,22 @@ class User extends BaseController
     {
         return view('user/landing');
     }
+    public function login(): string
+    {
+        return view('user/login');
+    }
+    public function signup(): string
+    {
+        return view('user/signup');
+    }
+    public function moodBoard(): string
+    {
+        return view('user/moodboard');
+    }
   
-
+    public function roadmap(): string
+    {
+        return view('user/roadmap');
+    }
+  
 }
