a0532b5136f90f4de2f32d155b9782d49ba01a41
Vian Tibay <202310591@fit.edu.ph>
2025-09-14 05:06:47 +0800

    fix(routes): Restore home route and remove unused landing page

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -5,7 +5,11 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+<<<<<<< HEAD
 // $routes->get('/', 'Home::index');
 $routes->get('/user','User::index');
 
 
+=======
+$routes->get('/', 'Home::index');
+>>>>>>> parent of 75716bb (feat(landing page): Added a new landing page)
